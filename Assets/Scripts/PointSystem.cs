73cd89e893890cf483104281d7979624be3a50f8
simcha33 <36536375+simcha33@users.noreply.github.com>
2018-09-22 13:39:18 +0200

    point system

---
+++ Assets/Scripts/PointSystem.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PointSystem : MonoBehaviour
+{
+    //Beweeg naar het object toe voor meer punten ga terug voor weer minder punten 
+
+    public bool onLand = true;
+    public bool boneZone = false;
+    public bool inWater = false; 
+
+    public int pointCounter = 0;
+    public int pointAdder= 1;
+    public int zoneBonus = 2; 
+
+    public float zoneDistance;
+    public float zoneBorder;
+    public float waterDistance; 
+
+    public Transform BoneZone;
+
+    public Text pointText;
+
+
+    void Start()
+    {
+        StartCoroutine("NormalePointTime");
+    }
+
+
+    void FixedUpdate()
+    {
+        zoneDistance = Vector3.Distance(BoneZone.position, transform.position);
+        pointText.text = ("" + pointCounter);
+
+        //checks zone 
+
+
+        if (inWater != true)
+        {
+            if (zoneDistance > zoneBorder)
+            {
+                boneZone = false;
+                onLand = true;
+            }
+
+
+            if (zoneDistance < zoneBorder)
+            {
+                boneZone = true;
+                onLand = false;
+            }
+        }   
+    }
+
+
+    IEnumerator NormalePointTime()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(1);
+
+            if (onLand == true)
+            {
+                pointCounter += pointAdder;
+            }
+
+            // add red zone bonus 
+            if (boneZone == true)
+            {
+                pointCounter += pointAdder + zoneBonus; 
+            }
+        }
+    }    
+}
+
+
